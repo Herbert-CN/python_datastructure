92d7fe43bc3b9ae8c66dfdf28e16bd2435092269
Herbert <4296176@qq.com>
2021-01-25 17:26:58 +0800

    Add files via upload

---
+++ backtrack/permutation.py
@@ -0,0 +1,26 @@
+
+class Solution:
+    def permutation(self, s: str):
+        visited = set()
+        result, subset = [], []
+        self.backtracking(result, visited, subset, s)
+        return result
+
+    def backtracking(self, result, visited, subset, s):
+        if len(subset) == len(s):
+            result.append(subset)
+
+        for ch in s:
+            if ch not in visited:
+                visited.add(ch)
+                self.backtracking(result, visited, subset + [ch], s)
+                visited.remove(ch)
+
+
+
+if __name__ == '__main__':
+    test = Solution()
+    result_final = test.permutation("abcdefg")
+    print(result_final)
+
+
