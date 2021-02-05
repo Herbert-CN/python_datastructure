fe934787aeb849201a500abba9905c197a78de0e
herbert zhou <4296176@qq.com>
2021-02-05 20:56:56 +0800

    Update code parenthess, beat 86%

--- backtrack/22. Generate Parentheses.py
+++ backtrack/22. Generate Parentheses.py
@@ -1,27 +1,25 @@
-"""
-Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
-
-
-
-Example 1:
-
-Input: n = 3
-Output: ["((()))","(()())","(())()","()(())","()()()"]
-Example 2:
-
-Input: n = 1
-Output: ["()"]
-
-
-Constraints:
-
-1 <= n <= 8
-"""
 from typing import List
 
 
 class Solution:
-    def generateParenthesis(self, n: int) -> List[str]:
-        pass
+    def getAllResults(self, n: int) -> List[str]:
+        result = []
+        self.dfs(0, 2*n, [], result)
+        return result
 
+    def dfs(self, curBalance, n, path, result):
+        if len(path) == n:
+            if curBalance == 0:
+                result.append(''.join(path))
+            return
+        if curBalance == 0:
+            self.dfs(curBalance + 1, n, path + ['('], result)
+        elif 0 < curBalance <= n - len(path):
+            self.dfs(curBalance - 1, n, path + [')'], result)
+            self.dfs(curBalance + 1, n, path + ['('], result)
+        else:
+            return
 
+if __name__ == '__main__':
+    test = Solution()
+    print(test.getAllResults(1))
